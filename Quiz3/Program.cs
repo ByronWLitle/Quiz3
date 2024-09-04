6c13b5e36c61e46f39f1d10cbfa085fe6add70d4
ByronWLitle <bwlitle@amarillocollege.com>
2024-09-04 12:59:34 -0500

    Add project files.

---
+++ Quiz3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Intialize variable to hold values for multiple iterations
+            string inputST = "";
+            string exit = "exit";
+            bool isPalindrome = true; 
+            //Title
+            Console.WriteLine("CHECK PALINDROME");
+            Console.WriteLine(""); //Divides space between title and prompt
+            do
+            {
+                //Prompts user for a string
+                Console.Write("Enter a string:");
+                //Declares user input as variable and store in memory
+                inputST = Console.ReadLine();
+
+                Console.WriteLine(""); //Divides space between prompts
+                for (int i = 0; i < inputST.Length; i++)
+                { 
+                    if (inputST[i] = inputST(inputST.Length-1))
+                    {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+                if (isPalindrome == true)
+                {
+                    Console.WriteLine($"Result: {inputST} is a palindrome.");
+                }
+                else
+                {
+                    Console.WriteLine($"Result: {inputST} is not a palindrome.");
+                }
+                Console.WriteLine(""); //Divides space between prompts
+                Console.WriteLine(""); //Divides space between prompts
+
+                //Prompts user for a string
+                Console.Write("Enter a string:");
+                //Declares user input as variable and store in memory
+                inputST = Console.ReadLine();
+            } while (inputST != exit);
+            Console.WriteLine("Bye!");
+        }
+    }
+}
